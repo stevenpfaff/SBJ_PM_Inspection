fd8b207683c3925ed4062be991ea300c5b1b65bb
stevenpfaff <pfaffy1426@gmail.com>
2022-03-18 10:43:04 -0500

    added bootstrap to first table on page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import './App.css'
+import { Table } from 'react-bootstrap'
+
 function App() {
   return (
     <div className="App">
-      <h1>SBJ Services LLC PM Inspection Checklist</h1>
-    </div>
+      <h1 style={{ textAlign: 'center', marginTop: '3%' }}>SBJ Services LLC PM Inspection Checklist</h1>
+      <h2 style={{ marginTop: '1%', marginLeft: '31%' }}>Work Order #</h2>
+      <div style={{ marginTop: '1%', marginLeft: '10%', marginRight: '10%' }}>
+        <Table striped bordered hover>
+          <thead>
+            <th>Customer</th>
+            <th>Unit #</th>
+            <th>VIN</th>
+            <th>Miles</th>
+            <th>Hours</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    </div >
   );
 }
 
